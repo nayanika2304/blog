6ddc5887397e329c469116cb68c5b6444970a911
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-08 15:28:07 -0500

    response normalization strategies over

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,8 +1,13 @@
 import express,{Response,Request,NextFunction} from "express";
+import {CustomError} from "../errors/custom-error";
 
 export const errorHandler = (err: Error,req:Request,res:Response,next:NextFunction) =>{
 
+    if(err instanceof CustomError){
+        return res.status(err.statusCode).send({errors:err.serializeErrors()});
+    }
+
     res.status(400).send({
-        message: err.message
+        errors: [{message: 'Something went wrong!'}]
     })
 }
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express,{Request,Response} from 'express';
 import {body,validationResult} from 'express-validator';
+import {RequestValidationError} from "../errors/request-validation-error";
+import {DatabaseConnectionError} from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -11,16 +13,17 @@ router.post('/api/users/signup',[
         .trim()
         .isLength({min:4,max:20})
         .withMessage('Password must be between length 4 and 20')
-],(req:Request,res:Response) =>{
+],async (req:Request,res:Response) =>{
     const errors = validationResult(req)
+
     if(!errors.isEmpty()){
-        throw new Error('Invalid email or password')
+        throw new RequestValidationError(errors.array())
     }
-    const {email,password} = req.body;
+
 
     console.log('Creating a user ...')
+    throw new DatabaseConnectionError()
 
-    res.send({})
 })
 
 export {router as signupRouter};
