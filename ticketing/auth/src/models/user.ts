5f9525ceedf506ad40d327ad87331e0a98eaee94
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-09 09:50:01 -0500

    created connection with mongoose using the kubernetes cluster and created a new User model using mongoose and typescript

---
+++ ticketing/auth/src/models/user.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+// An interface that describes the properties that are required to create a new user
+interface UserAttrs{
+    email: string,
+    password: string
+}
+
+//An  interface that describes the properties that a user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs:UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User document has
+interface UserDoc extends mongoose.Document{
+    email: string,
+    password: string
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+
+
+//custom way to add a function to the schema in mongoose
+userSchema.statics.build = (attrs: UserAttrs) =>{
+    return new User(attrs)
+}
+const User = mongoose.model<UserDoc,UserModel>('User',userSchema)
+
+const user = User.build({
+    email: 'test@test.com',
+    password: 'wjkhdakjd'
+})
+
+export {User}
