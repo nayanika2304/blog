3a700f235b66f726d1fc2aa97957a58c3271668c
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-09 10:01:56 -0500

    changes

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -37,9 +37,5 @@ userSchema.statics.build = (attrs: UserAttrs) =>{
 }
 const User = mongoose.model<UserDoc,UserModel>('User',userSchema)
 
-const user = User.build({
-    email: 'test@test.com',
-    password: 'wjkhdakjd'
-})
 
 export {User}
