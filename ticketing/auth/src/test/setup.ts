145b7fdf310b3fc9d4f319445ccd6c42650d89f5
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-11-15 14:11:10 -0500

    testing done

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,7 +1,15 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import {app} from "../app";
+import request from 'supertest';
 
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>
+        }
+    }
+}
 let mongo : any
 
 // a hook that runs before all test
@@ -18,7 +26,6 @@ beforeAll(async() =>{
 //runs before each of the test
 beforeEach( async() =>{
     const collections = await mongoose.connection.db.collections();
-
     for(let collection of collections){
         await collection.deleteMany({});
     }
@@ -29,3 +36,16 @@ afterAll(async () =>{
     await mongo.stop();
     await mongoose.connection.close()
 })
+
+global.signin = async() =>{
+    const email = 'test@test.com'
+    const password = 'password'
+    const response  = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,password
+        })
+        .expect(201)
+    const cookie = response.get('Set-Cookie');
+    return cookie
+}
